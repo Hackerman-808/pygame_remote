403875d25f753ac21278b9c5c37c0a5a307b15b6
Hackerman-808 <addressforitstuff@gmail.com>
2025-01-16 15:44:22 +0100

    fixed install, added constants.py, end of day 1

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+
+
+if __name__ == "__main__":
+    main()
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}") # type: ignore
